99a90fa09c1083a0b1ea36cf8d225d2c945ba5fa
Kirill Vnukov <kirillgrandson@gmail.com>
2024-02-21 11:07:28 +0300

    Added a login/registration controller class

---
+++ app/Http/Controllers/Auth/LoginRegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class LoginRegisterController extends Controller
+{
+    public function __construct() {}
+
+    public function register() {}
+
+    public function store() {}
+
+    public function login() {}
+
+    public function authenticate() {}
+
+    public function dashboard() {}
+
+    public function logout() {}
+}
